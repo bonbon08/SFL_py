a3c570f31ab9ea18036d8304978e67d73a8682e3
Bonbon <mail@jan-schmitt.eu>
2024-04-27 23:29:15 +0200

    added some features to gui

--- main.py
+++ main.py
@@ -9,25 +9,32 @@ effektivclass = {
 }
 
 class Player():
-    def __init__(self, CharacterClass, image):
+    def __init__(self, CharacterClass, idle_image1, idle_image2):
         if not checkOk(CharacterClass):
             exit()
         self.PC = CharacterClass
         self.PH = 100
         self.level = 1
         self.xp = 0
-        self.image = image
+        self.idle_image1 = idle_image1
+        self.idle_image2 = idle_image2
         self.Defnse = 1
+        self.X = 200
+        self.Y = 200
 
 class Enemy():
-    def __init__(self, CharacterClass, maxHP):
+    def __init__(self, CharacterClass, maxHP, idle_image1, idle_image2):
         if not checkOk(CharacterClass):
             exit()
         self.PC = CharacterClass
         self.xp = random.randint(1,20)
         self.level = 1
+        self.idle_image1 = idle_image1
+        self.idle_image2 = idle_image2
         self.PH = random.randint(50, maxHP)
         self.Defnse = 1
+        self.X = 200
+        self.Y = 200
 
 def checkOk(Pclass):
     if Pclass in avclassses:
@@ -100,20 +107,37 @@ if __name__=="__main__":
     pygame.display.set_caption("Hypermux")
     pygame.display.set_allow_screensaver(False)
     pygame.display.set_icon(applogo)
-    running = True
     arena1_background = loadimage("arena1.png", 800,600)
     start_background = loadimage("startscreen.png", 800, 600)
-    char1 = loadimage("water_mage_idle_1.png", 128, 128)
-    char2 = loadimage("fire_mage_idle_1.png", 128, 128)
-    char3 = loadimage("plant_mage_idle_1.png", 128, 128)
+    water_idle_1 = loadimage("water_mage_idle_1.png", 128, 128)
+    water_idle_2 = loadimage("water_mage_idle_2.png", 128, 128)
+    fire_idle_1 = loadimage("fire_mage_idle_1.png", 128, 128)
+    fire_idle_2 = loadimage("fire_mage_idle_2.png", 128, 128)
+    plant_idle_1 = loadimage("plant_mage_idle_1.png", 128, 128)
+    plant_idle_2 = loadimage("plant_mage_idle_2.png", 128, 128)
+    lama_idle_1 = loadimage("lama_idle_1.png", 128, 128)
+    lama_idle_2 = loadimage("lama_idle_2.png", 128, 128)
+    fireball = loadimage("fireball.png", 32, 32)
+    grassbob = loadimage("grass.png", 32, 32)
+    bg = arena1_background
     startscreen = True
     selectchar = True
-    textcharselect = my_font.render('Please Chose a Character', False, (255, 255, 255))
+    momfight = False
+    running = True
+    ticker = 0
     charselectervar = random.randint(0,2)
     charselecterstringcords = [200, 336, 472]
+    textcharselect = my_font.render('Please Chose a Character', False, (255, 255, 255))
+    starttext1 = my_font.render('Game by Bonbon, Jakob, Patrick', False, (255, 255, 255))
+    starttext2 = my_font.render('In Corparation with', False, (255, 255, 255))
+    starttext3 = my_font.render('Special Thank to arGameplay', False, (255, 255, 255))
     while running:
         if startscreen:
+            pygame.display.set_caption("Hypermux -Startscreen")
             screen.blit(start_background, (0,0))
+            screen.blit(starttext1, (50,300))
+            screen.blit(starttext2, (200, 400))
+            screen.blit(starttext3, (50, 350))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -123,7 +147,16 @@ if __name__=="__main__":
             pygame.display.flip()
             clock.tick(10)
         elif selectchar:
+            pygame.display.set_caption("Hypermux -Character Selection")
             screen.fill((0,0,0))
+            if ticker >= 6:
+                momsprite1 = water_idle_2
+                momsprite2 = fire_idle_2
+                momsprite3 = plant_idle_2
+            else:
+                momsprite1 = water_idle_1
+                momsprite2 = fire_idle_1
+                momsprite3 = plant_idle_1
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -140,23 +173,66 @@ if __name__=="__main__":
                             charselectervar = 0
                     if event.key == pygame.K_RETURN:
                         selectchar = False
+                        momfight = True
                         if charselectervar == 0:
                             klasse = "water"
-                            image = char1
+                            idle_image1 = water_idle_1
+                            idle_image2 = water_idle_2
                         elif charselectervar == 1:
                             klasse = "fire"
-                            image = char2
+                            idle_image1 = fire_idle_1
+                            idle_image2 = fire_idle_2
                         else:
                             klasse = "plant"
-                            image = char3
-                        Playerclass = Player(klasse, image)
-            pygame.draw.rect(screen, (255,255,0), (charselecterstringcords[charselectervar], 300, 128, 128))
-            screen.blit(char1, (200,300))
-            screen.blit(char2, (336,300))
-            screen.blit(char3, (472,300))
+                            idle_image1 = plant_idle_1
+                            idle_image2 = plant_idle_2
+                        Player = Player(klasse, idle_image1, idle_image2)
+                        Enemy = Enemy("fire", 60, lama_idle_1, lama_idle_2)
+            pygame.draw.rect(screen, (100,255,0), (charselecterstringcords[charselectervar], 300, 128, 128))
+            if charselectervar == 0:
+                screen.blit(momsprite1, (200,300))
+                screen.blit(fire_idle_1, (336,300))
+                screen.blit(plant_idle_1, (472,300))
+            elif charselectervar == 1:
+                screen.blit(water_idle_1, (200,300))
+                screen.blit(momsprite2, (336,300))
+                screen.blit(plant_idle_1, (472,300))
+            elif charselectervar == 2:
+                screen.blit(water_idle_1, (200,300))
+                screen.blit(fire_idle_1, (336,300))
+                screen.blit(momsprite3, (472,300))
             screen.blit(textcharselect, (260, 200))
             pygame.display.flip()
             clock.tick(10)
+            if ticker == 10 :
+                ticker = 0
+            else: 
+                ticker += 1
+        elif momfight:
+            pygame.display.set_caption("Hypermux -Fight")
+            screen.blit(bg, (0,0))
+            if ticker > 30:
+                playerspr = Player.idle_image2
+                enemyspr = Enemy.idle_image2
+            else:
+                playerspr = Player.idle_image1
+                enemyspr = Enemy.idle_image2
+            for event in pygame.event.get():
+                        if event.type == pygame.QUIT:
+                            running = False
+                        elif event.type == pygame.KEYDOWN:
+                            if event.key == pygame.K_UP:
+                                pass
+                            elif event.key == pygame.K_DOWN:
+                                pass
+                            elif event.key == pygame.K_RETURN:
+                                pass
+            pygame.display.flip()
+            clock.tick(60)
+            if ticker == 60:
+                ticker = 0
+            else:
+                ticker += 1
         else:
             screen.blit(arena1_background, (0,0))
             for event in pygame.event.get():
@@ -176,24 +252,3 @@ if __name__=="__main__":
             pygame.display.flip()
             clock.tick(60)
     pygame.quit()
-    """oPlayer = Player(random.choice(avclassses))
-    oEnemy = Enemy(random.choice(avclassses), 60)
-    run = True
-    print("Start demo")
-    while run:
-        print("Your live: " + str(oPlayer.PH))
-        print("Enemy live: " + str(oEnemy.PH))
-        att = int(input("Your Attack\n==> "))
-        if att >4 or att <1:
-            print("Demo broke")
-            exit()
-        Won = selattack(att, oPlayer, oEnemy)
-        if Won == True:
-            print("You Won")
-            exit()
-        print("Your live: " + str(oPlayer.PH))
-        print("Enemy live: " + str(oEnemy.PH))
-        Won = selattack(random.randint(1,4), oEnemy, oPlayer)
-        if Won == True:
-            print("You Lost")
-            exit()"""
