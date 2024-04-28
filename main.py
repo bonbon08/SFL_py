5109c72121ad7d3ddf1acaab019f56e2c944c448
Bonbon <mail@jan-schmitt.eu>
2024-04-28 12:17:38 +0200

    added first two animations for Player

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ effektivclass = {
 }
 
 class Player():
-    def __init__(self, CharacterClass, idle_image1, idle_image2, Attacknames):
+    def __init__(self, CharacterClass, idle_image1, idle_image2, ball1_image, ball2_image, ball3_image, sword_image, Attacknames):
         if not checkOk(CharacterClass):
             exit()
         self.PC = CharacterClass
@@ -18,6 +18,10 @@ class Player():
         self.xp = 0
         self.idle_image1 = idle_image1
         self.idle_image2 = idle_image2
+        self.ball_image1 = ball1_image
+        self.ball_image2 = ball2_image
+        self.ball_image3 = ball3_image
+        self.sword_image = sword_image
         self.Defnse = 1
         self.X = 150
         self.Y = 400
@@ -117,19 +121,41 @@ if __name__=="__main__":
     start_background = loadimage("startscreen.png", 800, 600)
     water_idle_1 = loadimage("water_mage_idle_1.png", 128, 128)
     water_idle_2 = loadimage("water_mage_idle_2.png", 128, 128)
+    water_ball_1 = loadimage("water_mage_ball_1.png", 128, 128)
+    water_ball_2 = loadimage("water_mage_ball_2.png", 128, 128)
+    water_ball_3 = loadimage("water_mage_ball_3.png", 128, 128)
+    water_sword = loadimage("water_sword.png", 64, 64)
     fire_idle_1 = loadimage("fire_mage_idle_1.png", 128, 128)
     fire_idle_2 = loadimage("fire_mage_idle_2.png", 128, 128)
+    fire_ball_1 = loadimage("fire_mage_ball_1.png", 128, 128)
+    fire_ball_2 = loadimage("fire_mage_ball_2.png", 128, 128)
+    fire_ball_3 = loadimage("fire_mage_ball_3.png", 128, 128)
+    fire_sword = loadimage("fire_sword.png", 64, 64)
     plant_idle_1 = loadimage("plant_mage_idle_1.png", 128, 128)
     plant_idle_2 = loadimage("plant_mage_idle_2.png", 128, 128)
+    plant_ball_1 = loadimage("plant_mage_ball_1.png", 128, 128)
+    plant_ball_2 = loadimage("plant_mage_ball_2.png", 128, 128)
+    plant_ball_3 = loadimage("plant_mage_ball_3.png", 128, 128)
+    plant_sword = loadimage("plant_sword.png", 64, 64)
     lama_idle_1 = loadimage("lama_idle_1.png", 128, 128)
     lama_idle_2 = loadimage("lama_idle_2.png", 128, 128)
-    fireball = loadimage("fireball.png", 32, 32)
-    grassbob = loadimage("grass.png", 32, 32)
+    fireball = loadimage("fire_ball.png", 32, 32)
+    grassball = loadimage("grass_ball.png", 32, 32)
+    waterball = loadimage("water_ball.png", 32, 32)
+    bow = loadimage("bow.png", 64, 64)
+    arrow = loadimage("arrow.png", 64, 64)
     bg = arena1_background
+    playanimation = False
+    animation_type = 0
+    character_mov = False
+    character_mov_back = False
+    animation_done = False
     startscreen = True
     selectchar = True
     momfight = False
     running = True
+    objektX = 220
+    objekt_show = False
     won = False
     ticker = 0
     Attboard = Attboard()
@@ -187,17 +213,29 @@ if __name__=="__main__":
                             Attacknames = ["Sword", "Arrow", "Waterblob", "Shield"]
                             idle_image1 = water_idle_1
                             idle_image2 = water_idle_2
+                            ball1_image = water_ball_1
+                            ball2_image = water_ball_2
+                            ball3_image = water_ball_3
+                            sword_image = water_sword
                         elif charselectervar == 1:
                             klasse = "fire"
                             Attacknames = ["Sword", "Arrow", "Fireball", "Shield"]
                             idle_image1 = fire_idle_1
                             idle_image2 = fire_idle_2
+                            ball1_image = fire_ball_1
+                            ball2_image = fire_ball_2
+                            ball3_image = fire_ball_3
+                            sword_image = fire_sword
                         else:
                             klasse = "plant"
                             Attacknames = ["Sword", "Arrow", "Dirtball", "Shield"]
                             idle_image1 = plant_idle_1
                             idle_image2 = plant_idle_2
-                        Player = Player(klasse, idle_image1, idle_image2, Attacknames)
+                            ball1_image = plant_ball_1
+                            ball2_image = plant_ball_2
+                            ball3_image = plant_ball_3
+                            sword_image = plant_sword
+                        Player = Player(klasse, idle_image1, idle_image2, ball1_image, ball2_image, ball3_image, sword_image, Attacknames)
                         Enemy = Enemy("fire", 60, lama_idle_1, lama_idle_2)
             pygame.draw.rect(screen, (100,255,0), (charselecterstringcords[charselectervar], 300, 128, 128))
             if charselectervar == 0:
@@ -219,55 +257,128 @@ if __name__=="__main__":
                 ticker = 0
             else: 
                 ticker += 1
-        elif momfight:
-            pygame.display.set_caption("Hypermux -Fight")
+        elif playanimation:
             screen.blit(bg, (0,0))
-            pygame.draw.rect(screen, (0,92,152), (50,50,300,200))
-            pygame.draw.rect(screen, (70,172,0), (50, Attboard.posses[Attboard.counter], 300,50))
-            for i in range(0,4):
-                text  = my_font.render(Player.Attacknames[i], False, (255, 255, 255))
-                screen.blit(text, (50, Attboard.posses[i]))
-            if ticker > 30:
-                playerspr = Player.idle_image2
-                enemyspr = Enemy.idle_image2
-            else:
-                playerspr = Player.idle_image1
-                enemyspr = Enemy.idle_image1
-            screen.blit(enemyspr, (Enemy.X, Enemy.Y))
             screen.blit(playerspr, (Player.X, Player.Y))
-            text  = my_font.render(str(Player.PH) + " HP", False, (204, 100, 30))
-            screen.blit(text, (180, 300 ))
-            text  = my_font.render(str(Enemy.PH) + " HP", False, (204, 100, 30))
-            screen.blit(text, (600, 300 ))
+            if animation_type == 0 :
+                enemyspr = Enemy.idle_image1
+                if Attboard.counter == 0:
+                    if ticker <= 10:
+                        playerspr = Player.idle_image1
+                    elif ticker <= 20:
+                        playerspr = Player.ball_image1
+                    elif ticker <= 30:
+                        playerspr = Player.ball_image2
+                    elif ticker <= 40:
+                        playerspr = Player.ball_image3
+                    else:
+                        playerspr = Player.ball_image3
+                        screen.blit(Player.sword_image, (Player.X+70, Player.Y+25))
+                        character_mov = True
+                    if character_mov == True:
+                        if character_mov_back == False:
+                            Player.X += 16
+                            if Player.X >= Enemy.X-128:
+                                character_mov_back = True
+                        else:
+                            Player.X -= 16
+                            if Player.X <= 150:
+                                Player.X = 150
+                                ticker = 360
+                elif Attboard.counter == 1:
+                    if ticker <= 10:
+                        playerspr = Player.idle_image1
+                    elif ticker <= 20:
+                        playerspr = Player.ball_image1
+                    elif ticker <= 30:
+                        playerspr = Player.ball_image2
+                    elif ticker <= 40:
+                        playerspr = Player.ball_image3
+                    else:
+                        playerspr = Player.ball_image3
+                        screen.blit(bow, (Player.X+70, Player.Y+25))
+                        screen.blit(arrow, (objektX, Player.Y+25))
+                        objekt_show = True
+                        character_mov = True
+                    if objekt_show == True:
+                        objektX += 16
+                        if objektX >= Enemy.X:
+                            ticker = 360
             for event in pygame.event.get():
                         if event.type == pygame.QUIT:
                             running = False
-                        elif event.type == pygame.KEYDOWN:
-                            if event.key == pygame.K_UP:
-                                if Attboard.counter == 0:
-                                    Attboard.counter = 3
-                                else: 
-                                    Attboard.counter -= 1
-                            elif event.key == pygame.K_DOWN:
-                                if Attboard.counter == 3:
-                                    Attboard.counter = 0
-                                else: 
-                                    Attboard.counter += 1
-                            elif event.key == pygame.K_RETURN:
-                                Player, Enemy = selattack(Attboard.counter+1, Player, Enemy)
-                                Enemy, Player = selattack(random.randint(1,4), Enemy, Player)
-            if Player.PH <= 0 :
-                momfight = False
-                won = False
-            elif Enemy.PH <= 0:
-                momfight = False
-                won = True
+            screen.blit(enemyspr, (Enemy.X, Enemy.Y))
             pygame.display.flip()
-            clock.tick(60)
-            if ticker == 60:
+            clock.tick(30)
+            if ticker == 360:
+                playanimation = False
+                animation_done = True
+                objektX = 220
+                character_mov = False
+                character_mov_back = False
+                objekt_show = False
                 ticker = 0
             else:
                 ticker += 1
+        elif momfight:
+            if animation_done == True and animation_type == 0:
+                playanimation = True
+                animation_done = False
+                animation_type = 1
+                Player, Enemy = selattack(Attboard.counter+1, Player, Enemy)
+            elif animation_done == True and animation_type == 1:
+                animation_done = False
+                Enemy, Player = selattack(random.randint(1,4), Enemy, Player)
+            else:
+                pygame.display.set_caption("Hypermux -Fight")
+                screen.blit(bg, (0,0))
+                pygame.draw.rect(screen, (0,92,152), (50,50,300,200))
+                pygame.draw.rect(screen, (70,172,0), (50, Attboard.posses[Attboard.counter], 300,50))
+                for i in range(0,4):
+                    text  = my_font.render(Player.Attacknames[i], False, (255, 255, 255))
+                    screen.blit(text, (50, Attboard.posses[i]))
+                if ticker > 30:
+                    playerspr = Player.idle_image2
+                    enemyspr = Enemy.idle_image2
+                else:
+                    playerspr = Player.idle_image1
+                    enemyspr = Enemy.idle_image1
+                screen.blit(enemyspr, (Enemy.X, Enemy.Y))
+                screen.blit(playerspr, (Player.X, Player.Y))
+                text  = my_font.render(str(Player.PH) + " HP", False, (204, 100, 30))
+                screen.blit(text, (180, 300 ))
+                text  = my_font.render(str(Enemy.PH) + " HP", False, (204, 100, 30))
+                screen.blit(text, (600, 300 ))
+                for event in pygame.event.get():
+                            if event.type == pygame.QUIT:
+                                running = False
+                            elif event.type == pygame.KEYDOWN:
+                                if event.key == pygame.K_UP:
+                                    if Attboard.counter == 0:
+                                        Attboard.counter = 3
+                                    else: 
+                                        Attboard.counter -= 1
+                                elif event.key == pygame.K_DOWN:
+                                    if Attboard.counter == 3:
+                                        Attboard.counter = 0
+                                    else: 
+                                        Attboard.counter += 1
+                                elif event.key == pygame.K_RETURN:
+                                    playanimation = True
+                                    ticker = 1
+                                    animation_type = 0
+                if Player.PH <= 0 :
+                    momfight = False
+                    won = False
+                elif Enemy.PH <= 0:
+                    momfight = False
+                    won = True
+                pygame.display.flip()
+                clock.tick(60)
+                if ticker == 60:
+                    ticker = 0
+                else:
+                    ticker += 1
         else:
             screen.blit(arena1_background, (0,0))
             for event in pygame.event.get():
